d470f6f286b6bb8053bb438bd11b97c66ff40ff6
Karolis_Stoncius_Sneakybox <karolis.stoncius@sneakybox.biz>
2023-08-08 11:53:35 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-header role="banner"> header </el-header>
+  <el-main>
+    <el-button text @click="dialogVisible = true">
+      click to open the Dialog
+    </el-button>
+    <el-dialog v-model="dialogVisible" title="Tips" width="30%" append-to-body>
+      <span>This is a message</span>
+    </el-dialog>
+  </el-main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+};
+</script>
