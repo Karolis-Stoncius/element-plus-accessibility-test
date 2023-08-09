ce52748e0f70d58bb758666cbd6b93e81b4a98c4
Karolis_Stoncius_Sneakybox <karolis.stoncius@sneakybox.biz>
2023-08-09 11:29:47 +0300

    Table and pagination examples

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <el-header role="banner"> header </el-header>
   <el-main>
+    <!-- Opening is dialog causes an accessibility issue - "Document should not have more than one banner landmark -->
     <el-button text @click="dialogVisible = true">
       click to open the Dialog
     </el-button>
     <el-dialog v-model="dialogVisible" title="Tips" width="30%" append-to-body>
-      <span>This is a message</span>
+      <span>text</span>
     </el-dialog>
+
+    <!-- Opening a filter dropdown in the table causes an accessibility issue - "All page content should be contained by landmarks" -->
+    <TableEx />
+
+    <!-- Opening a sizes dropdown causes an accessibility issue - "All page content should be contained by landmarks" -->
+    <el-pagination layout="sizes, prev, pager, next" :total="50" />
   </el-main>
 </template>
 
 <script>
+import TableEx from "./components/TableEx.vue";
 export default {
+  components: {
+    TableEx,
+  },
+
   data() {
     return {
       dialogVisible: false,
